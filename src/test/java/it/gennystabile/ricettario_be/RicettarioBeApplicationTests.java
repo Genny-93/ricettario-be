decdb1a0efdbceca9b407851062bf939f8cb8e2d
Genny <gennystabile1993@gmail.com>
2026-04-29 12:21:40 +0200

    first commit

---
+++ src/test/java/it/gennystabile/ricettario_be/RicettarioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gennystabile.ricettario_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicettarioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
